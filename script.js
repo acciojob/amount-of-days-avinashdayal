b8561e7796568d51daf199daa17ae6032693fd26
Avinash Dayal <avinashdayal1608@gmail.com>
2025-09-04 19:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Function to return days in a given year
+function days_of_a_year(year) {
+  return isLeapYear(year) ? 366 : 365;
+}
+
+// Function to check leap year
+function isLeapYear(year) {
+  return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+// Take input
+rl.question(' ', (input) => {
+  const year = parseInt(input);
+  console.log(` ${year}: ${days_of_a_year(year)}`);
+  rl.close();
+});
