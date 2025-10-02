625347f718384497a7b44d6a9b9ee1c40856f15f
Avinash Dayal <avinashdayal1608@gmail.com>
2025-10-02 11:21:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 rl.question('Enter a year: ', (input) => {
   const year = parseInt(input);
   if (isNaN(year) || year < 1 || year > 9999) {
     console.log("Please enter a valid year between 1 and 9999.");
   } else {
-    console.log(`The year ${year} has ${days_of_a_year(year)} days.`);
+    console.log(`The year ${year} has ${getDaysInYear(year)} days.`);
   }
   rl.close();
 });
-function getDaysInYear(year){
-	const isleap = (year%4===0 && year%100 !===0)|| (year % 400 === 0);
 
+function getDaysInYear(year) {
+  const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
   return isLeap ? 366 : 365;
 }
